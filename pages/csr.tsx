a008f35c7d7caf5819680309ebf7db645c836801
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-27 16:40:37 -0500

    CSR

--- pages/csr.tsx
+++ pages/csr.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function CSR() {
+  const [posts, setPosts] = useState<
+    {
+      id: number; 
+      title: string;
+      body: string;
+    }[]
+  >([]);
+
+  // El USEEFFECT Solo funciona del lado del cliente
+  useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => setPosts(data.slice(0, 5)))
+  }, []);
+
+
   return (
-    <div>CSR</div>
+    <div>
+      <h1>CSR: Client Side Rendering</h1>
+      {
+        posts.map(post => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        ))
+      }
+    </div>
   )
 }
