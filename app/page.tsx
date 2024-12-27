62df6231b355bbdbba83c40e98caf1f8e63a36dd
paul-ventura-git <j.paul.ventura@gmail.com>
2024-12-27 16:22:39 -0500

    Finished files

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="mt-10">Home</h1>
-      <p>Welcome to your new app!</p>
-    </>
-  );
-}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Index() {
   return (
     <div>
       <h1>Comparación de métodos de renderización</h1>
+      <ul>
+        <li>
+          <Link href="/CSR">CSR - Client Side Rendering</Link>
+        </li>
+        <li>
+          <Link href="/SSR">SSR - Server Side Rendering</Link>
+        </li>
+        <li>
+          <Link href="/SSG">SSG - Static Site Generation</Link>
+        </li>
+        <li>
+          <Link href="/ISR">ISR - Incremental Static Regeneration</Link>
+        </li>
+      </ul>
     </div>
   )
 }
